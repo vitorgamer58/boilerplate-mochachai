33eab01d6c5b6bcd9562cf28010ea3fb6fcf4269
Vitor Araujo <vitorgamer58@outlook.com>
2021-07-27 16:57:15 +0000

    all functional-tests passing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -75,16 +75,24 @@ suite("Functional Tests with Zombie.js", function () {
     // #5
     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
       browser.fill("surname", "Colombo").pressButton("submit", function () {
-        //assert.fail();
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.elements('span#dates', 1);
 
         done();
       });
     });
     // #6
     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
-      //assert.fail();
+      browser.fill("surname", "Vespucci").pressButton("submit", function () {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Amerigo');
+        browser.assert.text('span#surname', 'Vespucci');
+        browser.assert.elements('span#dates', 1);
 
-      done();
+        done();
+      })
     });
   });
 });
